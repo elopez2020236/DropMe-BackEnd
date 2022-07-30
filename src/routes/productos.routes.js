3d1d05ec9e446031ac653634d44ff0c3fddc3449
Genrry Uwualdo Luis Alvarado <88805705+gluis2020232@users.noreply.github.com>
2022-07-30 02:33:41 -0600

    Editar Producto Full

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -9,5 +9,6 @@ api.put("/editarProducto/:idProductos", controladorProductos.editarProducto)
 api.delete("/eliminarProducto/:idProductos", md_autenticacion.Auth, controladorProductos.eliminarProudcto);
 api.get("/obtenerProductos", controladorProductos.ObtenerProductosMain);
 api.get("/obtenerProductosLog", md_autenticacion.Auth, controladorProductos.ObtenerLog);
+api.get('/producto/:idProducto', controladorProductos.obtenerxId);
 
 module.exports = api;
